bbc1f724a132dec8df9e049e408b1d3e885c39da
LauraBerrio <blauritab@gmail.com>
2022-01-31 15:56:49 -0500

    cambios buscado

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,42 @@
-import { url1 } from "./url.js";
+import {url1} from "./url.js";
 import {changeTheme} from "./theme.js";
-import { getCountry } from "./getCountries.js"; 
-import { showCards, showModal } from "./showCards.js";
-import { search } from "./search.js";
+import {getCountry} from "./getCountries.js";
+import {showCards, showModal} from "./showCards.js";
+import {search} from "./search.js";
 
-
-const theme=document.getElementById("theme")
+const theme = document.getElementById("theme")
 const showCountry = document.getElementById('country');
 const btnFilter = document.getElementById('dropdownMenuButton1');
-const searchInput= document.getElementById("input");
+const searchInput = document.getElementById("input");
 
-document.addEventListener('DOMContentLoaded', async()=>{
+document.addEventListener('DOMContentLoaded', async () => {
     const data = await getCountry(url1);
     showCards(data, showCountry);
 })
-btnFilter.addEventListener('click', async(e) =>{    
-    const filterR = e.target.classList.contains('dropdown-item')    
+btnFilter.addEventListener('click', async (e) => {
+    const filterR = e.target.classList.contains('dropdown-item')
     const region = e.target.id
-     if(filterR){          
+    if (filterR) {
         const data = await getCountry(url1)
-        console.log( data)
-        let filterRegion = data.filter(c => c.region.toLocaleLowerCase() === region.toLocaleLowerCase()); 
-        showCards(filterRegion, showCountry)  
-        
-     }    
-     
+        console.log(data)
+        let filterRegion = data.filter(c => c.region.toLocaleLowerCase() === region.toLocaleLowerCase());
+        showCards(filterRegion, showCountry)
+    }
 })
 
-theme.addEventListener('click',()=>{    
+theme.addEventListener('click', () => {
     changeTheme()
 })
-showCountry.addEventListener('click', async(e) =>{
+showCountry.addEventListener('click', async (e) => {
     console.log(e)
     const data = await getCountry(url1);
-    if(e.target.classList.contains('mostrar')){
+    if (e.target.classList.contains('mostrar')) {
         let idM = e.target.id;
         showModal(data, idM)
     }
-    
-}
-)
-searchInput.addEventListener('keyup', async()=>{
+
+})
+searchInput.addEventListener('keyup', async () => {
     const data = await getCountry(url1);
-    // search(data);
-    let input= document.getElementById("input").value
-    // let i 
-    console.log(input)
-    data.forEach(countryF => {
-        // let title = cards[i].querySelector(".card-body h5.card-title");
-        if (countryF.country.toLocaleLowerCase().indexOf(input) > -1) {
-            showCards()
-        } else {
-            console.log('no encontrado')
-        }
-    });
+    search(data, showCountry);    
 })
--- scripts/search.js
+++ scripts/search.js
@@ -1,17 +1,32 @@
     
-export function search(data){
-    let  cardContainer = document.getElementById("country")
-    let cards = cardContainer.getElementsByClassName("card");
-    // var els = document.querySelectorAll(".el");
-    let input= document.getElementById("input").value
-    let i 
-    console.log(input)
-    data.forEach(countryF => {
-        let title = cards[i].querySelector(".card-body h5.card-title");
-        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
-            cards[i].style.display = "";
+export function search(data, showCountry){
+    let input = document.getElementById("input").value    
+    data.forEach(countryF => {        
+        const {
+            id,
+            country,
+            population,
+            region,
+            capital,
+            image
+        } = countryF
+        let searched = country.toLocaleLowerCase();        
+        if (searched.indexOf(input) !== -1) {            
+            showCountry.innerHTML = ""
+            showCountry.innerHTML += `
+            <div class="card">
+            <div class="card-body">
+                <a href="#exampleModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img id="${id}" src="${image}" alt="" class="img-thumbnail my-4 mostrar" style=" height:200px; width:343px">
+                </a>
+                <h5>${country}</h5>
+                <h6 id="population">${population}</h6>
+                <h6 id="region">${region}</h6>
+                <h6 id="capital">${capital}</h6>
+            </div>
+        </div>`
         } else {
-            cards[i].style.display = "none";
+            console.log('no encontrado')
         }
     });
   
